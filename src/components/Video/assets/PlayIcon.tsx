7a668420d146a3037a32b731aabce8c0d9a1262f
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-07-10 23:02:34 -0300

    feat: add Video component

---
+++ src/components/Video/assets/PlayIcon.tsx
@@ -0,0 +1,24 @@
+export const PlayIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 186 186"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-12 h-12 md:h-[11.5875rem] md:w-[11.5875rem]"
+      aria-label="Ícone representando o inicio da reprodução"
+    >
+      <title>Ícone representando o inicio da reprodução</title>
+      <circle
+        cx="93.2699"
+        cy="92.7786"
+        r="89.6986"
+        stroke="#8133F1"
+        strokeWidth="6"
+      />
+      <path
+        d="M59.3399 34.01L161.13 92.7786L59.3399 151.547L59.3399 34.01Z"
+        fill="#9654F4"
+      />
+    </svg>
+  );
+};
